c621239c8b6dc36415cfe52a39e1f8d91158285f
chengzee <as0780962@gmail.com>
2020-07-07 21:39:21 +0800

    add Sleep mode

---
+++ LoRaNodeWithDHT22_SleepTime/LoRaNodeWithDHT22_SleepTime.ino
@@ -0,0 +1,128 @@
+/*
+  LoRa Simple Node
+  Temperature and Humidity sensor: DHT 22
+  Light sensor:BH1750
+  Transition module: LoRa sx1278  
+*/
+
+#include <SPI.h>
+#include <LoRa.h>
+#include <BH1750.h>
+#include <Wire.h>
+#include "DHT.h"
+#include <LSleep.h>
+
+#define DHTPIN GPIO36
+#define DHTTYPE DHT22
+
+DHT dht(DHTPIN, DHTTYPE);
+BH1750 lightMeter;
+LSleepClass Sleep;
+
+const long frequency = 433E6;         // LoRa Frequency
+const int txPower = 20;               // LoRa TxPower
+const int spreadingfactor = 11;       // LoRa SpreadingFactor
+const long signalbandwidth = 125E3;  // LoRa SignalBandwidth      
+int counter = 1;
+float h, t, lux;
+
+void enterSleep(int _ms, int _mode)
+{
+  float _sleepTime = _ms * 0.001;
+  Sleep.init(true);
+  Sleep.setTime(_ms);       // 設定時間
+  Sleep.setMode(_mode);     // 睡眠模式：0 不睡；1 待機狀態；2 wifi睡眠狀態；3 傳統睡眠狀態
+  Serial.print("晚安(");
+  Serial.print(_sleepTime, 3);
+  Serial.println(" s)");
+  Sleep.sleep();
+  Serial.println("起床");
+}
+
+void setup() {
+  Serial.begin(9600);                 // initialize serial
+  while (!Serial);
+  Serial.println("LoRa Node");
+  
+  if (!LoRa.begin(frequency)) {
+    Serial.println("LoRa init failed. Check your connections.");
+    while (true);                      // if failed, do nothing
+  }else{
+    Serial.println("Great, you have init LoRa");
+  }
+  LoRa.setTxPower(txPower);
+  LoRa.setSpreadingFactor(spreadingfactor);
+  LoRa.setSignalBandwidth(signalbandwidth);
+}
+
+void loop() {
+  // start the dht22
+  dht.begin();
+  // initialize the i2c bus (bh1750's communicate interface)
+  Wire.begin();
+  // start the bh1750
+  lightMeter.begin();
+  // Wait a few seconds between measurements.
+  delay(1000);
+  // Reading temperature or humidity takes about 250 milliseconds!
+  // Sensor readings may also be up to 2 seconds 'old' (its a very slow sensor)
+  float h = dht.readHumidity();
+  // Read temperature as Celsius (the default)
+  float t = dht.readTemperature();
+  // Read light strengh in lux (lm/m^2)
+  float lux = lightMeter.readLightLevel();
+  Serial.print(h);
+  Serial.print(",");
+  Serial.print(t);
+  Serial.print(",");
+  Serial.println(lux);
+  // Check if any reads failed and exit early (to try again).
+  if (isnan(h) || isnan(t) || h >= 100 || h <= 0 || t >= 40 || t <= 0 ) {
+    Serial.println("Failed to read from DHT sensor!");
+    return;
+  }
+  if (isnan(lux) || lux > 65535 || lux < 0) {
+    Serial.println("Failed to read from bh1750 sensor!");
+    return;
+  }
+  // ------------------------------------------------------------------------------
+  // Send
+  //  start lora
+  for (int i = 0; i < 5; i++){
+    if (!LoRa.begin(frequency)) {
+      Serial.println("LoRa init failed. Check your connections.");
+      continue;                    // if failed, try again
+    }else{
+      Serial.println("Great, you have init LoRa");
+      break;
+    }
+  }
+  LoRa.setTxPower(txPower);
+  LoRa.setSpreadingFactor(spreadingfactor);
+  LoRa.setSignalBandwidth(signalbandwidth);
+  
+  Serial.print("Sending packet: ");
+  Serial.print("Node1, ");
+  Serial.print(counter);
+  Serial.print(", ");
+  Serial.print(h);
+  Serial.print(", ");
+  Serial.print(t);
+  Serial.print(", ");
+  Serial.println(lux);
+  if (counter>1){
+    // send packet
+    LoRa.beginPacket();
+    LoRa.print("Node1, ");
+    LoRa.print(counter);
+    LoRa.print(", ");
+    LoRa.print(h);
+    LoRa.print(", ");
+    LoRa.print(t);
+    LoRa.print(", ");
+    LoRa.print(lux);
+    LoRa.endPacket();
+  }
+  counter++;
+  enterSleep(300000,3);
+}
