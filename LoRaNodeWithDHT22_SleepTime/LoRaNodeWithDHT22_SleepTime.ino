ebec790ddf79cf646b125690bf9dccb87ceea52f
chengzee <as0780962@gmail.com>
2020-07-22 23:35:31 +0800

    add library reference

--- LoRaNodeWithDHT22_SleepTime/LoRaNodeWithDHT22_SleepTime.ino
+++ LoRaNodeWithDHT22_SleepTime/LoRaNodeWithDHT22_SleepTime.ino
@@ -2,7 +2,10 @@
   LoRa Simple Node
   Temperature and Humidity sensor: DHT 22
   Light sensor:BH1750
-  Transition module: LoRa sx1278  
+  Transition module: LoRa sx1278
+  ---------------------------------------------------------------------------------------------------------
+  Libraries:
+  SleepMode Reference:https://github.com/kylecat/Linklt7697/tree/6b4a03c8847fbf19e8854060e9d0a313c25d7765  
 */
 
 #include <SPI.h>
