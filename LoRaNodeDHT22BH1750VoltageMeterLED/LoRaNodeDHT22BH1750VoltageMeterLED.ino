5baac4fdb37138752f847311bb6dc4ea09498bba
chengzee <as0780962@gmail.com>
2020-11-09 15:49:21 +0800

    fix some bug

--- LoRaNodeDHT22BH1750VoltageMeterLED/LoRaNodeDHT22BH1750VoltageMeterLED.ino
+++ LoRaNodeDHT22BH1750VoltageMeterLED/LoRaNodeDHT22BH1750VoltageMeterLED.ino
@@ -12,11 +12,11 @@
 #include "DHT.h"
 #include <LSleep.h>
 
-#define DHTPIN 4
+#define DHTVPIN 4
+#define DHTPIN 5
 #define DHTTYPE DHT22
-//#define powerSwitch 5
-#define DHTVPIN 5
-#define RED_LED 0
+#define analogInput 14  // read divided voltage
+#define digitalOutput 3 // set voltagemeteray on/off 
 
 DHT dht(DHTPIN, DHTTYPE);
 BH1750 lightMeter;
@@ -32,12 +32,8 @@ const int spreadingfactor = 11;       // LoRa SpreadingFactor
 const long signalbandwidth = 125E3;  // LoRa SignalBandwidth      
 int counter = 1;
 float h, t, lux;
-const int analogInput = 14;
-float vout = 0.0;
-float vin = 0.0;
-float R1 = 30000.0; //  
-float R2 = 7500.0; // 
-int value = 0;
+float vout;
+float voltage = 0;
 
 void enterSleep(int _ms, int _mode)
 {
@@ -56,64 +52,58 @@ void setup() {
   Serial.begin(9600);                 // initialize serial
   while (!Serial);
   Serial.println("LoRa Node");
-  pinMode(irqPin, OUTPUT);                  // 7
-  pinMode(DHTVPIN, OUTPUT);                 // 5
-  pinMode(RED_LED, OUTPUT);                 // 0
+  pinMode(irqPin, OUTPUT);
+  pinMode(DHTVPIN, OUTPUT);
   pinMode(analogInput, INPUT);              // 14
+  pinMode(digitalOutput, OUTPUT);           // 3 voltagemeter
+  // set initial state
+  digitalWrite(DHTVPIN, LOW);
+  digitalWrite(digitalOutput, LOW);
 }
 
 void loop() {
   //-----------------------------------------------------------------
-  // turn on the power
-//  digitalWrite(powerSwitch, HIGH);
-  digitalWrite(DHTVPIN, HIGH);
-  
-  //-----------------------------------------------------------------
+  // if it work all day, we measure the voltage  
   // read the value at analog input
-  value = analogRead(analogInput);
-  Serial.print("value(in pin 2):");
-  Serial.println(value);
-  vout = (value * 2.5) / 4096.0;
-  vin = vout / (R2/(R1+R2)); 
-  Serial.print("INPUT V= ");
-  Serial.println(vin,2);
-  
-  // ------------------------------------------------------------------------------
-  // Check Input Voltage is still enough
-  if (vin < 3.5){
-    digitalWrite(RED_LED, HIGH);    
+  if (counter%5 == 1){
+    // turn on the voltagemeter's power by setting relay
+    digitalWrite(digitalOutput, HIGH);
+    int value = analogRead(analogInput);
+    Serial.print("value(in pin 14):");
+    Serial.println(value);
+    vout = (value * 2.5) / 4096.0;
+    voltage = vout / (7500.0/(30000.0+7500.0)); 
+    Serial.print("INPUT V= ");
+    Serial.println(voltage,2);
+    //-----------------------------------------------------------------
+    // turn off the voltagemeter's power when we complete measure
+    digitalWrite(digitalOutput, LOW);
+    // Check Input Voltage is still enough
+    if (voltage < 3.2){
+      digitalWrite(digitalOutput, HIGH); 
+    }
+    else{
+      digitalWrite(digitalOutput, HIGH);
+      delay(500);
+      digitalWrite(digitalOutput, LOW);
+      delay(500);
+      digitalWrite(digitalOutput, HIGH);
+      delay(500);
+      digitalWrite(digitalOutput, LOW);
+      delay(500);
+    }
   }
-  else{
-    digitalWrite(RED_LED, HIGH);
-    delay(500);
-    digitalWrite(RED_LED, LOW);
-    delay(500);
-    digitalWrite(RED_LED, HIGH);
-    delay(500);
-    digitalWrite(RED_LED, LOW);
-    delay(500);
-    digitalWrite(RED_LED, HIGH);
-    delay(500);
-    digitalWrite(RED_LED, LOW);
-    delay(500);
-  }
-
-  //-----------------------------------------------------------------
-  // Set LoRa pins
   LoRa.setPins(csPin, resetPin, irqPin);
-  
-  //-----------------------------------------------------------------
+  digitalWrite(DHTVPIN, HIGH);
   // start the DHT22
   dht.begin();
   // Wait a few seconds between measurements.
   delay(1000);
-  //-----------------------------------------------------------------
   // Reading temperature or humidity takes about 250 milliseconds!
   // Sensor readings may also be up to 2 seconds 'old' (its a very slow sensor)
   float h = dht.readHumidity();
   // Read temperature as Celsius (the default)
   float t = dht.readTemperature();
-  //-----------------------------------------------------------------
   // Read light strengh in lux (lm/m^2)
   // initialize the i2c bus (bh1750's communicate interface)
   Wire.begin();
@@ -125,7 +115,6 @@ void loop() {
   Serial.print(t);
   Serial.print(",");
   Serial.println(lux);
-  //-----------------------------------------------------------------
   // Check if any reads failed and exit early (to try again).
   if (isnan(h) || isnan(t)) {
     Serial.println("Failed to read from DHT sensor!");
@@ -139,10 +128,11 @@ void loop() {
     Serial.println("Failed to read from bh1750 sensor!");
     return;
   }
+  digitalWrite(DHTVPIN, LOW);
   
   // ------------------------------------------------------------------------------
   // Send
-  // Start lora
+  //  start lora
   for (int i = 0; i < 5; i++){
     if (!LoRa.begin(frequency)) {
       Serial.println("LoRa init failed. Check your connections.");
@@ -157,7 +147,7 @@ void loop() {
   LoRa.setSignalBandwidth(signalbandwidth);
   
   Serial.print("Sending packet: ");
-  Serial.print("Node91, ");
+  Serial.print("Node94, ");
   Serial.print(counter);
   Serial.print(", ");
   Serial.print(h);
@@ -165,17 +155,12 @@ void loop() {
   Serial.print(t);
   Serial.print(", ");
   Serial.println(lux);
-//  Serial.print(", ");
-//  Serial.println(vin);
-  
-  // ------------------------------------------------------------------------------
-  // Sending packet(turn LoRa power on)
   digitalWrite(irqPin, HIGH);
+ 
   if (counter>1){
     // send packet
-    
     LoRa.beginPacket();
-    LoRa.print("Node91, ");
+    LoRa.print("Node94, ");
     LoRa.print(counter);
     LoRa.print(", ");
     LoRa.print(h);
@@ -183,15 +168,11 @@ void loop() {
     LoRa.print(t);
     LoRa.print(", ");
     LoRa.print(lux);
-//    LoRa.print(", ");
-//    LoRa.print(vin);
     LoRa.endPacket();
   }
-  Serial.println("1, flag");
+  
   digitalWrite(irqPin, LOW);
-  digitalWrite(DHTVPIN, LOW);
-//  digitalWrite(powerSwitch, LOW);
   counter++;
   enterSleep(293645,3);
-//  enterSleep(7730,3);
+//  enterSleep(3645,3);
 }
